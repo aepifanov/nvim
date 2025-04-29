00bba6fc5c0ca6965e504f7123ed926eccb06d15
Andrey Epifanov <aepifanov@mirantis.com>
2025-04-29 16:33:02 -0700

    Add keymap for C-k and C-j

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map("n", "<C-k>", "<C-u>", { desc = "Go to Up", remap = true })
+map("n", "<C-j>", "<C-d>", { desc = "Go to Down", remap = true })
